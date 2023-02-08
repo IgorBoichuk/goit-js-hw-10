0fa77fad4d00214294a44c1c459a35095d5a173d
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-08 23:00:29 +0200

    321

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+export { fetchCountries };
+
+function fetchCountries(name) {
+  if (inputCountries.value === '') {
+    return;
+  }
+  countryName = name.target.value;
+
+  return fetch(
+    `https://restcountries.com/v3.1/name/${countryName}?fields=,name,capital,population,flags,languages`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(country => console.log(country)); //сюда прикрутить функцию для рендера на страницу
+}
+console.log(fetchCountries());
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
+import { debounce } from 'lodash';
+import Notiflix from 'notiflix';
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+const inputCountries = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+// console.log(inputCountries);
+//  1:01 інтерфейс в окремій функції
+
+// const DEBOUNCE_DELAY = 300;
+
+// const debounced = debounce(() => {
+//   console.log('delayed');
+// }, DEBOUNCE_DELAY);
+
+inputCountries.addEventListener('input', fetchCountries);
+console.log(inputCountries.value);
+
+//   .then(renderSingleCountry)
+//   .catch(error => console.log(error));
+
+function makingSingleCountry(countries) {
+  const markupCountryCard = countries.map(
+    ({ flags, name, capital, population, languages }) => {
+      return `<div class="country-div"><img src=${flags.svg} alt = ${
+        flags.alt
+      } width = 50/><h2 class="country-singl-name">${
+        name.common
+      }</h2></div><ul><li class="country-property"><span>Capital:</span> ${capital}</li><li class="country-property"><span>Population:</span> ${population}</li><li class="country-property"> <span>Languages:</span> ${Object.values(
+        languages
+      )}</li></ul>`;
+    }
+  );
+  console.log(markupCountryCard);
+}
+
+function makingCountriesList(countries) {}
+
+function renderCountryToHtml(countries) {}
