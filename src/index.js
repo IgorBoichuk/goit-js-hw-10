8f96a6024d9c87a4d24f0aff84ecd35fe7e94c2c
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-02-10 15:36:04 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,78 +1,66 @@
-import { debounce } from 'lodash';
-import Notiflix from 'notiflix';
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
-// import { fetchCountries } from './fetchCountries'; // -----------заімпортити з файлу і прибрати ф-цію нижче
-
-const inputCountries = document.querySelector('#search-box');
+const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+const DEBOUNCE_DELAY = 300;
 
-//  1:01 інтерфейс в окремій функції
-
-inputCountries.addEventListener('input', debounce(fetchCountries, 300));
-// Якщо користувач повністю очищає поле пошуку, то HTTP-запит не виконується,
-// а розмітка списку країн або інформації про країну зникає.
-
-function fetchCountries(name) {
-  console.log(inputCountries.value.trim());
-  // прибрати цю ф-цію і заімпортити з файлу-----fetchCountries.js----------------------------------------
-  if (inputCountries.value.trim() === '') {
-    countryInfo.innerHTML = '';
-  } else countryName = inputCountries.value.trim();
-
-  return fetch(
-    `https://restcountries.com/v3.1/name/${countryName}?fields=,name,capital,population,flags,languages`
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(country => {
-      if (country.length === 1) {
-        makingSingleCountry(country);
-      } else makingCountriesList(country);
-    });
-}
-
-// Якщо у відповіді бекенд повернув більше ніж 10 країн, в інтерфейсі з'являється
-// повідомлення про те, що назва повинна бути специфічнішою. Для повідомлень використовуй
-// бібліотеку notiflix і виводь такий рядок "Too many matches found. Please enter a more specific name."
-
-// Якщо бекенд повернув від 2-х до 10-и країн, під тестовим полем відображається список знайдених країн.
-// Кожен елемент списку складається з прапора та назви країни.
-
-function makingSingleCountry(country) {
-  const markupCountryCard = country.map(
-    ({ flags, name, capital, population, languages }) => {
-      return `<div class="country-div"><img src=${flags.svg} alt = ${
-        flags.alt
-      } width = 50/><h2 class="country-singl-name">${
-        name.common
-      }</h2></div><ul><li class="country-property"><span>Capital:</span> ${capital}</li><li class="country-property"><span>Population:</span> ${population}</li><li class="country-property"> <span>Languages:</span> ${Object.values(
-        languages
-      )}</li></ul></div>`;
+input.addEventListener(
+  'input',
+  debounce(e => {
+    const trimmedValue = input.value.trim();
+    cleanHtml();
+    if (trimmedValue !== '') {
+      fetchCountries(trimmedValue).then(foundData => {
+        if (foundData.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (foundData.length === 0) {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+        } else if (foundData.length >= 2 && foundData.length <= 10) {
+          renderCountryList(foundData);
+        } else if (foundData.length === 1) {
+          renderOneCountry(foundData);
+        }
+      });
     }
-  );
+  }, DEBOUNCE_DELAY)
+);
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" hight="20">
+         <b>${country.name.official}</p>
+                </li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
+
+function renderOneCountry(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${
+        country.name.official
+      }" width="30" hight="20">
+         <b>${country.name.official}</b></p>
+            <p><b>Capital</b>: ${country.capital}</p>
+            <p><b>Population</b>: ${country.population}</p>
+            <p><b>Languages</b>: ${Object.values(country.languages)} </p>
+                </li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
+
+function cleanHtml() {
   countryList.innerHTML = '';
-  joinHtml = markupCountryCard.join('');
-  countryInfo.insertAdjacentHTML('afterbegin', joinHtml);
-  // console.log(arrayCountryInfo);
-}
-
-function makingCountriesList(country) {
-  const markupCountryList = country.map(({ flags, name }) => {
-    return `<li class = "country-item">${name.common}</li>`;
-  });
   countryInfo.innerHTML = '';
-
-  joinHtml = markupCountryList.join('');
-  countryList.insertAdjacentHTML('afterbegin', joinHtml);
 }
-
-// Якщо користувач ввів назву країни, якої не існує, бекенд поверне не порожній масив,
-// а помилку зі статус кодом 404 - не знайдено. Якщо це не обробити, то користувач
-// ніколи не дізнається про те, що пошук не дав результатів. Додай повідомлення
-// "Oops, there is no country with that name" у разі помилки, використовуючи бібліотеку notiflix.
-
-// Не забувай про те, що fetch не вважає 404 помилкою, тому необхідно явно відхилити
-// проміс, щоб можна було зловити і обробити помилку.
